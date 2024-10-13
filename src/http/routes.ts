28cb2a8dc12bcf8cacb73f1ab32f06e512b3a007
Pedro <pedrodelmonico@hotmail.com>
2024-10-13 17:57:32 -0300

    feat: adding jwt

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,11 @@
-import { authenticate } from './controllers/authenticate'
+import { authenticate } from './controllers/authenticate.controller'
+import { profile } from './controllers/profile.controller'
 import { register } from './controllers/register.controller'
 import { FastifyInstance } from 'fastify'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.get('/me', profile)
 }
